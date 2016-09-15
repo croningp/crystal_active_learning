71c8b397f559d6239fc3ad97f6808aa3a79cfa0f
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-09-15 11:34:12 +0100

    random data + plot

--- analysis/learning_curves.py
+++ analysis/learning_curves.py
@@ -30,18 +30,11 @@ matplotlib.rc('xtick', labelsize=20)
 matplotlib.rc('ytick', labelsize=20)
 matplotlib.rcParams.update({'font.size': fontsize})
 
+N_INIT_POINTS = 89
 
-if __name__ == '__main__':
-
-    filename = os.path.join(root_path, 'real_experiments', 'uncertainty', '0', '0010', 'data.csv')
-
-    N_INIT_POINTS = 89
-    test_range = range(0, 101, 10)
+def compute_learning_curve(filename, (X_test, y_test), test_range=range(0, 101, 10)):
 
     X, y = read_data(filename)
-
-    all_X = X
-    all_y = y
 
     scores = []
     for t in test_range:
@@ -51,16 +44,46 @@ if __name__ == '__main__':
         y_train = y[0:ind]
 
         clf = train_classifier(X_train, y_train)
-        prediction_accuracy = clf.score(all_X, all_y)
+        prediction_accuracy = clf.score(X_test, y_test)
         scores.append(prediction_accuracy)
+
+    return test_range, scores
+
+
+def get_init_data(filename):
+    X, y = read_data(filename)
+    return X[:N_INIT_POINTS, :], y[:N_INIT_POINTS]
+
+
+def get_new_data(filename):
+    X, y = read_data(filename)
+    return X[N_INIT_POINTS:, :], y[N_INIT_POINTS:]
+
+
+if __name__ == '__main__':
+
+    random_filename = os.path.join(root_path, 'real_experiments', 'random', '0', 'data.csv')
+
+    uncertainty_filename = os.path.join(root_path, 'real_experiments', 'uncertainty', '0', '0010', 'data.csv')
+
+    X_test, y_test = read_data(random_filename)
+    X, y = get_new_data(uncertainty_filename)
+    X_test = np.vstack((X_test, X))
+    y_test = np.hstack((y_test, y))
+
+    test_range, r_scores = compute_learning_curve(random_filename, (X_test, y_test))
+    test_range, u_scores = compute_learning_curve(uncertainty_filename, (X_test, y_test))
+
 
 
     fig = plt.figure(figsize=(12, 8))
-    plt.plot(test_range, scores)
+    plt.plot(test_range, r_scores)
+    plt.plot(test_range, u_scores)
+    plt.legend(['Random', 'Uncertainty'], fontsize=fontsize)
     plt.xlim([0, 100])
     plt.ylim([0.5, 1])
     plt.xlabel('Number of experiments', fontsize=fontsize)
     plt.ylabel('Prediction accuracy', fontsize=fontsize)
 
-    plot_filename = os.path.join(HERE_PATH, 'plot', 'uncertainty')
+    plot_filename = os.path.join(HERE_PATH, 'plot', 'learning_curve')
     save_and_close_figure(fig, plot_filename, exts=['.png'])
